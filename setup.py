1a402069466f757bbba2c87a1360199503642ecb
Andrew Rollings <a.rollings@hiive.com>
2019-10-01 14:36:44 -0400

    Was broken with latest packages.
    setup.py tweaks.

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ setup(
     author='M. Simpson, J. McGehee',
     author_email= 'mjs2600@gmail.com, jlmcgehee21@gmail.com',
     packages=['mimicry'],
-#     scripts=['bin/example1.py','bin/example2.py'], #Any scripts we may want
-#     url='http://pypi.python.org/pypi/mimicry/', #If we actually want to upload this to PyPI
     license='MIT', #Unless you prefer another
     description='MIMIC Randomized Optimization Algorithm in Python',
     long_description='MIMIC Randomized Optimization Algorithm in Python',
     install_requires=['numpy', 'scipy', 'scikit-learn', 'pandas', 'networkx', 'matplotlib'],
     python_requires='>=3',
+    zip_safe=False
 )
